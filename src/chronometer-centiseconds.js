d7aa5553299ac25660cb80a0aa92d3061ed160fa
Eric Masip Vilalta <ericmasipvilalta@gmail.com>
2024-10-17 21:56:52 +0200

    ready to start with iteration 9

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,18 +1,28 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+      printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   getCentiseconds() {
@@ -21,17 +31,24 @@ class Chronometer {
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    const valueExtended = `0${value}`;        
+    return valueExtended.slice(-2);
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    const minString = this.computeTwoDigitNumber(this.getMinutes())
+    const secString = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minString}:${secString}`
   }
 }
